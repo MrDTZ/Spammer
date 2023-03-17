263a08a40b455c3533fcc124355496a9cb3da870
Mr.Devil (Owner of DTZ) <118705843+MrDTZ@users.noreply.github.com>
2023-03-17 09:12:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+# 😈️ Spammer 😈️ 
+
+
+- 📌️ This is Whatsapp spaming tol
+
+- 📌️ Only Work in Kali Linux,Windows and Android.
+
+
+# 📥️ Install for kali Linux
+
+- 📌️ $ sudo apt update
+
+- 📌️ $ sudo apt upgrade
+
+- 📌️ $ git clone 
+
+- 📌️ $ cd Spammer
+
+- 📌️ $ pip install pyautogui
+
+- 📌️ $ pip install colorama
+
+- 📌️ $ python3 main.py
+
+- ⚠️ Select the Spam in menu after Open Whatsapp Web and Open Chat you wnat to spam !.
+
+# 📥️ Install For Termux (Android)
+
+- 📌️ $ apt update -y
+
+- 📌️ $ apt upgrade -y
+
+- 📌️ $ apt install git -y
+
+- 📌️ $ apt install python3 -y
+
+- 📌️ $ git clone 
+
+- 📌️ $ cd Spammer
+
+- 📌️ $ pip install pyautogui
+
+- 📌️ $ pip install colorama
+
+- 📌️ $ python3 main.py
+
+- ⚠️ Select the Spam in menu after Open Whatsapp app and Open Chat you wnat to spam !.
+
+
+# credits 
+
+- ⭐️ Tool By Mr.Devil.
+
+- ⭐️ Dark Tool Zone SriLanka 🇱🇰️.
+
+
+# 🙋‍♂️️ Have a Good Day !
---
+++ main.py
@@ -0,0 +1,90 @@
+import colorama
+from colorama import Fore
+import pyautogui
+import time
+
+print('')
+
+print(Fore.RED + 'Welcome to Spammer!')
+print("")
+
+print(Fore.GREEN + '░██████╗██████╗░░█████╗░███╗░░░███╗███╗░░░███╗███████╗██████╗░')
+print(Fore.GREEN + '██╔════╝██╔══██╗██╔══██╗████╗░████║████╗░████║██╔════╝██╔══██╗')
+print(Fore.GREEN + '╚█████╗░██████╔╝███████║██╔████╔██║██╔████╔██║█████╗░░██████╔╝')
+print(Fore.GREEN + '░╚═══██╗██╔═══╝░██╔══██║██║╚██╔╝██║██║╚██╔╝██║██╔══╝░░██╔══██╗')
+print(Fore.GREEN + '██████╔╝██║░░░░░██║░░██║██║░╚═╝░██║██║░╚═╝░██║███████╗██║░░██║', Fore.BLUE + '[*]', Fore.YELLOW + "Dark Tool Zone Sri Lanka")
+print(Fore.GREEN + '╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝', Fore.BLUE + '[*]', Fore.YELLOW + "Tool By Mr.Devil (Yohan Deshitha)")
+
+print("")
+
+def menu():
+    print(Fore.RED + " [1]. Spam01 \n [2]. Spam02 \n [3]. Spam03 \n [4]. Spam04")
+    option = input()
+
+    if option == "1":
+      print(Fore.BLUE + "[+] Spam is started in 15 second !")
+      print("")
+      print(Fore.RED + "[+] Open Whatsapp Web and Open Chat you wnat to spam !")
+      print("")
+      print(Fore.YELLOW + "[+] Spam Credits -:", Fore.BLUE + "Lasal" )
+      print("")
+      f=open("script_1", 'r')
+
+      time.sleep(15)
+
+      for word in f:
+         pyautogui.typewrite(word)
+         pyautogui.press("Enter")
+
+    if option == "2":
+      print(Fore.BLUE + "[+] Spam is started in 15 second !")
+      print("")
+      print(Fore.RED + "[+] Open Whatsapp Web and Open Chat you wnat to spam !")
+      print("")
+      print(Fore.YELLOW + "[+] Spam Credits -:", Fore.BLUE + "Lasal" )
+      print("")
+      f=open("script_2", 'r')
+
+      time.sleep(15)
+
+      for word in f:
+         pyautogui.typewrite(word)
+         pyautogui.press("Enter")
+         
+
+    if option == "3":
+      print(Fore.BLUE + "[+] Spam is started in 15 second !")
+      print("")
+      print(Fore.RED + "[+] Open Whatsapp Web and Open Chat you wnat to spam !")
+      print("")
+      print(Fore.YELLOW + "[+] Spam Credits -:", Fore.BLUE + "B.S.Prabasha" )
+      print("")
+      f=open("script_3", 'r')
+
+      time.sleep(15)
+
+      for word in f:
+         pyautogui.typewrite(word)
+         pyautogui.press("Enter")
+         
+    if option == "4":
+      print(Fore.BLUE + "[+] Spam is started in 15 second !")
+      print("")
+      print(Fore.RED + "[+] Open Whatsapp Web and Open Chat you wnat to spam !")
+      print("")
+      print(Fore.YELLOW + "[+] Spam Credits -:", Fore.BLUE + "B.S.Prabasha" )
+      print("")
+      f=open("script_4", 'r')
+
+      time.sleep(15)
+
+      for word in f:
+         pyautogui.typewrite(word)
+         pyautogui.press("Enter")
+         
+
+menu()
+
+
+
+
